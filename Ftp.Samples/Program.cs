58e4fb0442b7b732176b4acc8e284f17bc050de6
xutian <xutian0521@qq.com>
2019-02-18 17:09:00 +0800

    add new Ftp.Samples

---
+++ Ftp.Samples/Program.cs
@@ -0,0 +1,32 @@
+﻿using Lvl.Tools;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ftp.Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 下载
+            FtpClient fc = new FtpClient("ftp://192.168.1.107/", "xt", "xt123456");
+            fc.Download("/1/2.gif", @"c:\4.jpg");
+            Console.WriteLine("下载完成！");
+            Console.ReadKey();
+            #endregion
+
+            #region 上传
+            FtpClient fc2 = new FtpClient("ftp://192.168.1.107/", "xt", "xt123456");
+            FileInfo fi=new FileInfo(@"c:\4.jpg");
+            fc.Upload(fi, "/1/5.jpg");
+            Console.WriteLine("上传完毕！");
+            Console.ReadKey();
+            #endregion
+            
+        }
+    }
+}
